15111dc24c0ac2eacc581164e56bf6136be445be
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2021-02-14 18:36:52 +0200

    fix nav

--- src/blocks/includes/nav/nav.js
+++ src/blocks/includes/nav/nav.js
@@ -1,7 +1,7 @@
 let enterNavButton = document.querySelector("#enterNavBtn");
 let naVmodal = document.querySelector(".nav-modal");
 let navList = document.querySelector(".nav__list");
-let selectedNavLink = document.querySelector(".nav__link.isActive");
+let selectedNavLink = document.querySelector(".nav__link_active");
 
 enterNavButton.onclick = function() {
   naVmodal.style.display = "block";
@@ -15,9 +15,9 @@ navList.onclick = function(e) {
 
 function activateNavLink(link) {
   if (selectedNavLink) {
-    selectedNavLink.classList.remove("isActive");
+    selectedNavLink.classList.remove("nav__link_active");
   }
 
   selectedNavLink = link;
-  selectedNavLink.classList.add("isActive");
+  selectedNavLink.classList.add("nav__link_active");
 }
