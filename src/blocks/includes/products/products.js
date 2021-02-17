cbf5c16d0fcc7d78d10beef99637a15cffea886a
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2021-02-17 19:38:06 +0200

    add opeCart and buildCurtList in products.js

--- src/blocks/includes/products/products.js
+++ src/blocks/includes/products/products.js
@@ -3,6 +3,11 @@ let productItems = document.querySelectorAll(".products__item");
 let buyButtons = document.querySelectorAll(".products__btn");
 let cartLabel = document.getElementById("cartLabel");
 let cartLabelTxt = cartLabel.children[1];
+let cartBtn = document.getElementById("cartBtn");
+let closeCartBtn = document.getElementById("closeCartBtn");
+
+let cartModal = document.querySelector(".cart-modal");
+let cartList = document.querySelector(".cart-modal__list");
 
 let compareButtons = document.querySelectorAll(".products-compare");
 let compareLabel = document.getElementById("compareLabel");
@@ -44,10 +49,10 @@ function addToCart() {
   this.lastChild.textContent = "В корзине";
 
   setCartData(cartData);
-  showCart(cartData);
+  showCartLabel(cartData);
 }
 
-function showCart(data) {
+function showCartLabel(data) {
   let sum = Object.values(data).reduce((sum, current) => sum + current[2], 0);
   cartLabelTxt.textContent = sum;
   cartLabel.style.display = "block";
@@ -57,97 +62,145 @@ buyButtons.forEach(function(buyButton) {
   buyButton.addEventListener("click", addToCart);
 });
 
-function getCompareData() {
-  return JSON.parse(localStorage.getItem("compare"));
-}
+function buildCartList() {
+  if (getCartData()) {
+    let cartData = getCartData();
+    let cartDataEntries = Object.entries(cartData);
 
-function setCompareData(item) {
-  localStorage.setItem("compare", JSON.stringify(item));
-  return false;
-}
+    let totalSum = 0;
+    let totelAmount = 0;
+    let totalItems =
+      '<table class="cart__list"><tr><th>Наименование</th><th>Цена</th><th>Кол-во</th></tr>';
 
-function addToCompare() {
-  let compareData = getCompareData() || {};
-  let parentBox = this.parentElement.parentElement.parentElement;
-  let itemId = this.getAttribute("data-id");
-  let itemTitle = parentBox.querySelector(".products__caption .products__title")
-    .innerHTML;
+    cartDataEntries.forEach((cartEntries) => {
+      let cartId = cartEntries[0];
+      let cartItem = cartEntries[1];
 
-  let itemPrice = parseInt(
-    parentBox
-      .querySelector(".products__prices .products__price--active")
-      .textContent.split(" ")
-      .join("")
-  );
+      totalItems += "<tr>";
+      totalSum += cartItem[1] * cartItem[2];
+      totelAmount += cartItem[2];
 
-  if (compareData.hasOwnProperty(itemId)) {
-    compareData[itemId][2] += 1;
+      cartItem.forEach((value) => {
+        totalItems += `<td>${value}</td>`;
+      });
+      totalItems += "</tr>";
+    });
+
+    totalItems += `<tr><th>Всего к оплате</th><th>${totalSum} грн.</th><th>${totelAmount} шт.</th></tr>`;
+    totalItems += "</table>";
+    cartList.innerHTML = totalItems;
   } else {
-    compareData[itemId] = [itemTitle, itemPrice, 1];
+    cartList.previousElementSibling.innerHTML = "Корзина пуста";
+    console.log(cartList.previousElementSibling);
   }
-
-  this.lastChild.textContent = "В сравнении";
-
-  setCompareData(compareData);
-  showCompare(compareData);
 }
 
-function showCompare(data) {
-  compareLabelTxt.textContent = Object.entries(data).length;
-  compareLabel.style.display = "block";
+function openCart() {
+  buildCartList();
+
+  cartModal.style.display = "block";
+  document.body.style.overflowY = "hidden";
 }
 
-compareButtons.forEach(function(compareButton) {
-  compareButton.addEventListener("click", addToCompare);
-});
-
-function getWishData() {
-  return JSON.parse(localStorage.getItem("wish"));
+function closeCart() {
+  cartModal.style.display = "none";
+  document.body.style.overflowY = "visible";
 }
 
-function setWishData(item) {
-  localStorage.setItem("wish", JSON.stringify(item));
-  return false;
-}
+cartBtn.addEventListener("click", openCart);
+closeCartBtn.addEventListener("click", closeCart);
 
-function addToWish() {
-  let wishData = getWishData() || {};
-  let parentBox = this.parentElement.parentElement.parentElement;
-  let itemId = this.getAttribute("data-id");
-  let itemTitle = parentBox.querySelector(".products__caption .products__title")
-    .innerHTML;
+// function getCompareData() {
+//   return JSON.parse(localStorage.getItem("compare"));
+// }
 
-  let itemPrice = parseInt(
-    parentBox
-      .querySelector(".products__prices .products__price--active")
-      .textContent.split(" ")
-      .join("")
-  );
+// function setCompareData(item) {
+//   localStorage.setItem("compare", JSON.stringify(item));
+//   return false;
+// }
 
-  if (wishData.hasOwnProperty(itemId)) {
-    wishData[itemId][2] += 1;
-  } else {
-    wishData[itemId] = [itemTitle, itemPrice, 1];
-  }
+// function addToCompare() {
+//   let compareData = getCompareData() || {};
+//   let parentBox = this.parentElement.parentElement.parentElement;
+//   let itemId = this.getAttribute("data-id");
+//   let itemTitle = parentBox.querySelector(".products__caption .products__title")
+//     .innerHTML;
 
-  this.lastChild.textContent = "В избранном";
+//   let itemPrice = parseInt(
+//     parentBox
+//       .querySelector(".products__prices .products__price--active")
+//       .textContent.split(" ")
+//       .join("")
+//   );
 
-  setWishData(wishData);
-  showWish(wishData);
-}
+//   if (compareData.hasOwnProperty(itemId)) {
+//     compareData[itemId][2] += 1;
+//   } else {
+//     compareData[itemId] = [itemTitle, itemPrice, 1];
+//   }
 
-function showWish(data) {
-  wishLabelTxt.textContent = Object.entries(data).length;
-  wishLabel.style.display = "block";
-}
+//   this.lastChild.textContent = "В сравнении";
 
-wishButtons.forEach(function(wishButton) {
-  wishButton.addEventListener("click", addToWish);
-});
+//   setCompareData(compareData);
+//   showCompare(compareData);
+// }
+
+// function showCompare(data) {
+//   compareLabelTxt.textContent = Object.entries(data).length;
+//   compareLabel.style.display = "block";
+// }
+
+// compareButtons.forEach(function(compareButton) {
+//   compareButton.addEventListener("click", addToCompare);
+// });
+
+// function getWishData() {
+//   return JSON.parse(localStorage.getItem("wish"));
+// }
+
+// function setWishData(item) {
+//   localStorage.setItem("wish", JSON.stringify(item));
+//   return false;
+// }
+
+// function addToWish() {
+//   let wishData = getWishData() || {};
+//   let parentBox = this.parentElement.parentElement.parentElement;
+//   let itemId = this.getAttribute("data-id");
+//   let itemTitle = parentBox.querySelector(".products__caption .products__title")
+//     .innerHTML;
+
+//   let itemPrice = parseInt(
+//     parentBox
+//       .querySelector(".products__prices .products__price--active")
+//       .textContent.split(" ")
+//       .join("")
+//   );
+
+//   if (wishData.hasOwnProperty(itemId)) {
+//     wishData[itemId][2] += 1;
+//   } else {
+//     wishData[itemId] = [itemTitle, itemPrice, 1];
+//   }
+
+//   this.lastChild.textContent = "В избранном";
+
+//   setWishData(wishData);
+//   showWish(wishData);
+// }
+
+// function showWish(data) {
+//   wishLabelTxt.textContent = Object.entries(data).length;
+//   wishLabel.style.display = "block";
+// }
+
+// wishButtons.forEach(function(wishButton) {
+//   wishButton.addEventListener("click", addToWish);
+// });
 
 window.addEventListener("load", function() {
   if (getCartData()) {
-    showCart(getCartData());
+    showCartLabel(getCartData());
     buyButtons.forEach(function(buyButton) {
       for (let key of Object.keys(getCartData())) {
         if (buyButton.getAttribute("data-id") == key) {
@@ -156,24 +209,24 @@ window.addEventListener("load", function() {
       }
     });
   }
-  if (getCompareData()) {
-    showCompare(getCompareData());
-    compareButtons.forEach(function(compareButton) {
-      for (let key of Object.keys(getCompareData())) {
-        if (compareButton.getAttribute("data-id") == key) {
-          compareButton.lastChild.textContent = "В сравнении";
-        }
-      }
-    });
-  }
-  if (getWishData()) {
-    showWish(getWishData());
-    wishButtons.forEach(function(wishButton) {
-      for (let key of Object.keys(getWishData())) {
-        if (wishButton.getAttribute("data-id") == key) {
-          wishButton.lastChild.textContent = "В избранном";
-        }
-      }
-    });
-  }
+  // if (getCompareData()) {
+  //   showCompare(getCompareData());
+  //   compareButtons.forEach(function(compareButton) {
+  //     for (let key of Object.keys(getCompareData())) {
+  //       if (compareButton.getAttribute("data-id") == key) {
+  //         compareButton.lastChild.textContent = "В сравнении";
+  //       }
+  //     }
+  //   });
+  // }
+  // if (getWishData()) {
+  //   showWish(getWishData());
+  //   wishButtons.forEach(function(wishButton) {
+  //     for (let key of Object.keys(getWishData())) {
+  //       if (wishButton.getAttribute("data-id") == key) {
+  //         wishButton.lastChild.textContent = "В избранном";
+  //       }
+  //     }
+  //   });
+  // }
 });
